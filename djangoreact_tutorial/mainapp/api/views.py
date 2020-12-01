7b5355eb2b4f7d3f77ece1eec880dab612c87bf7
edzen12 <oichiev.edzen@gmail.com>
2020-12-01 19:28:20 +0600

    commit for save

--- djangoreact_tutorial/mainapp/api/views.py
+++ djangoreact_tutorial/mainapp/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 
-from .serializers import BlogCategorySerializer, BlogPostSerializer
+from .serializers import BlogCategorySerializer, BlogPostSerializer, BlogPostListRetrieveSerializer
 from ..models import BlogCategory, BlogPost
 
 
@@ -12,3 +12,14 @@ class BlogCategoryViewSet(viewsets.ModelViewSet):
 class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+    action_to_serializer = {
+        "list": BlogPostListRetrieveSerializer,
+        "retrieve": BlogPostListRetrieveSerializer
+    }
+
+    def get_serializer_class(self):
+        return self.action_to_serializer.get(
+            self.action,
+            self.serializer_class
+        )
