254add1c5266de0869b5923858a871321da16832
edzen12 <oichiev.edzen@gmail.com>
2020-12-02 17:45:44 +0600

    commit for save

--- djangoreact_tutorial/mainapp/api/serializers.py
+++ djangoreact_tutorial/mainapp/api/serializers.py
@@ -9,6 +9,18 @@ class BlogCategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class BlogCategoryDetailSerializer(serializers.ModelSerializer):
+    posts = serializers.SerializerMethodField()
+
+    class Meta:
+        model = BlogCategory
+        fields = '__all__'
+
+    @staticmethod
+    def get_posts(obj):
+        return BlogPostSerializer(BlogPost.objects.filter(blog_category=obj), many=True).data
+
+
 class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
@@ -21,3 +33,5 @@ class BlogPostListRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = '__all__'
+
+
