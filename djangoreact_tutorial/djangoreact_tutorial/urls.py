e8d9d1a878edeb229264d77b090159c1de57e870
edzen12 <oichiev.edzen@gmail.com>
2020-12-05 16:07:23 +0600

    commit for save

--- djangoreact_tutorial/djangoreact_tutorial/urls.py
+++ djangoreact_tutorial/djangoreact_tutorial/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mainapp.views import index
+from mainapp.views import index, category_detail, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('category/<int:id>/', category_detail),
+    path('posts/<int:id>/', post_detail),
     path('api/', include('mainapp.api.urls'))
 ]
